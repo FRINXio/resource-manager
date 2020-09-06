73ebd9c9f56e78f5416c489b0a861a7a4ef20f3f
Maros Marsalek <mmarsalek@frinx.io>
2020-09-06 11:44:22 +0200

    RM: Add real IPv4 prefix allocation strategy
    Issue: NH-315
    Test Plan: Unit tests and manual testing via test-queries.graphql
    
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- pools/allocating_strategies/__tests__/ipv4_prefix_splin_in_2_strategy_test.js
+++
@@ -1,4 +0,0 @@
-test("prefix splitting", () => {
-    const strat = require('../ipv4_prefix_splin_in_2_strategy');
-    console.log(strat.invokeWithParams([], {'ResourcePoolName': "10.0.0.0/8"}));
-})
