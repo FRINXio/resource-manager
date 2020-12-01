93ed12e0e0947299ecae968ecb7ff7b3d6928275
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-12-01 18:02:35 +0100

    Add api-tests to GH actions

---
+++ api-tests/test.sh
@@ -0,0 +1,13 @@
+set -x
+dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${dirname}
+
+
+docker-compose -f docker-compose.api-tests.yaml up -d
+
+trap 'docker-compose -f docker-compose.api-tests.yaml down' err exit
+
+sleep 5
+cp .env-LOCAL-DEV-CONFIG .env
+yarn install
+yarn jest
