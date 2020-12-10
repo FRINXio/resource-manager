e9816b6fb7467465a0df9aa48c2cd0f0773d8341
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-12-10 15:17:37 +0100

    Lower logging in api-tests

--- api-tests/test.sh
+++ api-tests/test.sh
@@ -3,7 +3,6 @@ dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${dirname}
 
 docker-compose -f docker-compose.api-tests.yaml up -d
-trap 'docker-compose -f docker-compose.api-tests.yaml logs resource-manager' err exit
 
 sleep 5 # wait for postgres
 cp .env-LOCAL-DEV-CONFIG .env
