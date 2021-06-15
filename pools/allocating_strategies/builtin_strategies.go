683d5c5529b609392b7f20564749769fc5b3c614
Tomas <thodor@frinx.io>
2021-06-15 15:09:52 +0200

    Rename Formatter_counter_strategy to unique_id_strategy
    Signed-off-by: Tomas <thodor@frinx.io>

--- pools/allocating_strategies/builtin_strategies.go
+++ pools/allocating_strategies/builtin_strategies.go
@@ -1274,6 +1274,64 @@ function capacity() {
 
 `
 
+const UNIQUE_ID = `
+'use strict';
+
+// framework managed constants
+//;
+//;
+// framework managed constants
+
+// STRATEGY_START
+
+/*
+Unique id generator
+- this strategy accepts template as textFunction and will replace variables in {} for values
+- {counter} is static variable for iterations
+- example: 'VPN-{counter}-{network}-{vpn}-local'
+ */
+
+String.prototype.format = function(dict) {
+    return this.replace(/{(\w+)}/g, function(match, key) {
+        return typeof dict[key] !== 'undefined'
+            ? dict[key]
+            : match
+            ;
+    });
+};
+
+function getNextFreeCounter() {
+    let max = 0;
+    for (let i = 0; i < currentResources.length; i++) {
+        if (currentResources[i].Properties.counter > max) {
+            max = currentResources[i].Properties.counter;
+        }
+    }
+    return ++max;
+}
+
+// main
+function invoke() {
+    let nextFreeCounter = getNextFreeCounter();
+    if (resourcePoolProperties == null){
+        console.error("Unable to extract resources");
+        return null
+    }
+    const { textFunction, ...poolProperties } = resourcePoolProperties;
+    let textOutput = resourcePoolProperties["textFunction"].format(
+        {...{counter: nextFreeCounter}, ...poolProperties});
+    return { text: textOutput, counter: nextFreeCounter };
+}
+
+function capacity() {
+    let allocatedCapacity = getNextFreeCounter() - 1;
+    let freeCapacity = Number.MAX_SAFE_INTEGER - allocatedCapacity;
+    return { freeCapacity: freeCapacity, utilizedCapacity: allocatedCapacity };
+}
+
+
+`
+
 const VLAN_RANGE = `
 'use strict';
 
@@ -1518,38 +1576,3 @@ function capacity() {
 
 `
 
-const FORMATTER_COUNTER = `
-'use strict';
-
-// framework managed constants
-//;
-//;
-// framework managed constants
-
-// STRATEGY_START
-
-
-function  getNextFreeCounter() {
-    let max = 0;
-    for (let i = 0; i < currentResources.length; i++) {
-        if (currentResources[i].counter > max) {
-            max = currentResources[i].counter;
-        }
-    }
-
-    return ++max;
-}
-
-function invoke() {
-    let nextFreeCounter = getNextFreeCounter();
-    return {text: userInput.textFunction(userInput, nextFreeCounter, resourcePoolProperties), counter: nextFreeCounter};
-}
-
-function capacity() {
-    let allocatedCapacity = getNextFreeCounter() - 1;
-    let freeCapacity = Number.MAX_SAFE_INTEGER - allocatedCapacity;
-    return { freeCapacity: freeCapacity, utilizedCapacity: allocatedCapacity };
-}
-
-
-`
---
+++ pools/allocating_strategies/strategies/src/tests/unique_id.test.js
@@ -0,0 +1,66 @@
+const strat = require('../unique_id_strategy')
+
+test("unique id output and capacity", () => {
+    let allocated = [uniqueId(0, 'first'),
+        uniqueId(1, 'second'),
+        uniqueId(3, 'third')];
+
+    let resource_pool = {
+        vpn: "VPN85", network: "Network19",
+        textFunction: "VPN-{counter}-{network}-{vpn}-local"}
+
+    let output = strat.invokeWithParams(allocated, resource_pool, {});
+    expect(output)
+        .toStrictEqual({'counter': 4, text: 'VPN-4-Network19-VPN85-local'});
+
+    let capacity = strat.invokeWithParamsCapacity(allocated, resource_pool, {});
+    expect(capacity)
+        .toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 3, utilizedCapacity: 3});
+})
+
+test("no resource pool", () => {
+
+    let output = strat.invokeWithParams([], null, {});
+    expect(output).toStrictEqual(null);
+})
+
+test("simple l3vpn counter", () => {
+
+    let output = strat.invokeWithParams([],
+        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+    expect(output).toStrictEqual({'counter': 1, text: 'L3VPN1'});
+
+    let capacity = strat.invokeWithParamsCapacity([uniqueId(output.counter, output.text)],
+        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+    expect(capacity).toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 1, utilizedCapacity: 1});
+
+    let next_output = strat.invokeWithParams([uniqueId(output.counter, output.text)],
+        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+    expect(next_output).toStrictEqual({'counter': 2, text: 'L3VPN2'});
+
+    let next_capacity = strat.invokeWithParamsCapacity(
+        [uniqueId(output.counter, output.text), uniqueId(next_output.counter, next_output.text)],
+        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+    expect(next_capacity).toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 2, utilizedCapacity: 2});
+})
+
+test("multiple l3vpn counters", () => {
+
+    let outputs = []
+    for (let i = 1; i <= 10; i++) {
+        let unique_id = strat.invokeWithParams(outputs,
+            {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"},
+            {});
+        outputs.push(uniqueId(unique_id.counter, unique_id.text))
+        expect(unique_id).toStrictEqual({'counter': i, text: 'L3VPN'+ i })
+    }
+
+    let capacity = strat.invokeWithParamsCapacity(outputs,
+        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"},
+        {});
+    expect(capacity).toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 10, utilizedCapacity: 10});
+})
+
+function uniqueId(counter, text) {
+    return {"Properties": {"counter": counter, "text": text}}
+}
