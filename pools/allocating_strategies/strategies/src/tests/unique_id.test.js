b981c05f08f7d8483b7ae7caad8805e44b586228
Tomas <thodor@frinx.io>
2021-06-23 14:07:51 +0200

    Rename resourcePoolProperties in Unique-id strategy
    - changed property "textFuntion" to "idFormat"
    - "idFormat" is mandatory property in Unique-id strategy
    
    Signed-off-by: Tomas <thodor@frinx.io>

--- pools/allocating_strategies/strategies/src/tests/unique_id.test.js
+++ pools/allocating_strategies/strategies/src/tests/unique_id.test.js
@@ -7,7 +7,7 @@ test("unique id output and capacity", () => {
 
     let resource_pool = {
         vpn: "VPN85", network: "Network19",
-        textFunction: "VPN-{counter}-{network}-{vpn}-local"}
+        idFormat: "VPN-{counter}-{network}-{vpn}-local"}
 
     let output = strat.invokeWithParams(allocated, resource_pool, {});
     expect(output)
@@ -18,45 +18,54 @@ test("unique id output and capacity", () => {
         .toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 3, utilizedCapacity: 3});
 })
 
-test("no resource pool", () => {
-
+test("params without resourcePool", () => {
     let output = strat.invokeWithParams([], null, {});
     expect(output).toStrictEqual(null);
 })
 
-test("simple l3vpn counter", () => {
-
+test("resourcePool without idFormat", () => {
     let output = strat.invokeWithParams([],
-        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+        {someProperty: "SomeUniqueL3VPN"}, {});
+    expect(output).toStrictEqual(null)
+})
+
+test("idFormat without counter", () => {
+    let output = strat.invokeWithParams([],
+        {someProperty: "SomeUniqueL3VPN", idFormat: "{someProperty}"}, {});
+    expect(output).toStrictEqual(null)
+})
+
+test("simple l3vpn counter", () => {
+    let output = strat.invokeWithParams([],
+        {someProperty: "L3VPN", idFormat: "{someProperty}{counter}"}, {});
     expect(output).toStrictEqual({'counter': 1, text: 'L3VPN1'});
 
     let capacity = strat.invokeWithParamsCapacity([uniqueId(output.counter, output.text)],
-        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+        {someProperty: "L3VPN", idFormat: "{someProperty}{counter}"}, {});
     expect(capacity).toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 1, utilizedCapacity: 1});
 
     let next_output = strat.invokeWithParams([uniqueId(output.counter, output.text)],
-        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+        {someProperty: "L3VPN", idFormat: "{someProperty}{counter}"}, {});
     expect(next_output).toStrictEqual({'counter': 2, text: 'L3VPN2'});
 
     let next_capacity = strat.invokeWithParamsCapacity(
         [uniqueId(output.counter, output.text), uniqueId(next_output.counter, next_output.text)],
-        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"}, {});
+        {someProperty: "L3VPN", idFormat: "{someProperty}{counter}"}, {});
     expect(next_capacity).toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 2, utilizedCapacity: 2});
 })
 
 test("multiple l3vpn counters", () => {
-
     let outputs = []
     for (let i = 1; i <= 10; i++) {
         let unique_id = strat.invokeWithParams(outputs,
-            {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"},
+            {someProperty: "L3VPN", idFormat: "{someProperty}{counter}"},
             {});
         outputs.push(uniqueId(unique_id.counter, unique_id.text))
         expect(unique_id).toStrictEqual({'counter': i, text: 'L3VPN'+ i })
     }
 
     let capacity = strat.invokeWithParamsCapacity(outputs,
-        {someProperty: "L3VPN", textFunction: "{someProperty}{counter}"},
+        {someProperty: "L3VPN", idFormat: "{someProperty}{counter}"},
         {});
     expect(capacity).toStrictEqual({freeCapacity: Number.MAX_SAFE_INTEGER - 10, utilizedCapacity: 10});
 })
