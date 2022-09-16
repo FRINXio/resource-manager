fa2307deb7abe50c66437934656a607dcde5ae60
vklevlanyk <vklevlanyk@frinx.io>
2022-09-16 11:02:31 +0200

    Fix api tests
    Signed-off-by: vklevlanyk <vklevlanyk@frinx.io>

--- api-tests/test.sh
+++ api-tests/test.sh
@@ -3,7 +3,7 @@ set -xe
 dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${dirname}
 
-docker compose -f docker-compose.api-tests.yaml up -d
+docker-compose -f docker-compose.api-tests.yaml up -d
 
 sleep 5 # wait for postgres
 cp .env-LOCAL-DEV-CONFIG .env
