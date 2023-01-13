8487a1462c94d6c852f9e1249dd0465979960173
Marco <51787428+MarcoMruz@users.noreply.github.com>
2023-01-13 08:45:51 +0100

    optimalize desired value calc (#87)

--- pools/allocating_strategies/strategies/src/tests/ipv4_prefix_test.go
+++ pools/allocating_strategies/strategies/src/tests/ipv4_prefix_test.go
@@ -368,7 +368,7 @@ func TestClaimResourceWithDesiredValue(t *testing.T) {
 func TestClaimResourceWithInvalidDesiredValue(t *testing.T) {
 	var allocated []map[string]interface{}
 	var resourcePool = map[string]interface{}{"prefix": 24, "address": "192.168.1.0", "subnet": false}
-	var userInput = map[string]interface{}{"desiredSize": 8, "desiredValue": "192.168.1.30"}
+	var userInput = map[string]interface{}{"desiredSize": 8, "desiredValue": "192.168.1.31"}
 	ipv4PrefixStruct := src.NewIpv4Prefix(allocated, resourcePool, userInput)
 	output, _ := ipv4PrefixStruct.Invoke()
 
@@ -392,7 +392,7 @@ func TestClaimResourceWithoutDesiredValue(t *testing.T) {
 }
 
 func TestClaimResourceInPoolWithAllocatedResources(t *testing.T) {
-	var allocated = []map[string]interface{}{ipv4Prefix("192.168.1.40", 29, false), ipv4Prefix("192.168.1.0", 29, false)}
+	var allocated = []map[string]interface{}{ipv4Prefix("192.168.1.128", 29, false), ipv4Prefix("192.168.1.78", 29, false), ipv4Prefix("192.168.1.40", 29, false), ipv4Prefix("192.168.1.0", 29, false)}
 	var resourcePool = map[string]interface{}{"prefix": 24, "address": "192.168.1.0", "subnet": false}
 	var userInput = map[string]interface{}{"desiredSize": 8, "desiredValue": "192.168.1.16"}
 	ipv4PrefixStruct := src.NewIpv4Prefix(allocated, resourcePool, userInput)
