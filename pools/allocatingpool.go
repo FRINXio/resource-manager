9faf3589a888a75d16850b8d53dfd3e506614498
Marco Mrúz <marcomruz1@gmail.com>
2023-08-14 15:28:13 +0200

    add commit of tx to allocating pool to not block pipeline of next txs

--- pools/allocatingpool.go
+++ pools/allocatingpool.go
@@ -206,6 +206,12 @@ func (pool AllocatingPool) Capacity() (string, string, error) {
 			return "0", "0", errors.Wrapf(err, "Unable to open new read transaction for pool %d", pool.ID)
 		}
 		pool.ctx = context.WithValue(pool.ctx, ent.TxCtxKey{}, tx)
+		defer func(tx *ent.Tx) {
+			err := tx.Commit()
+			if err != nil {
+				log.Error(pool.ctx, err, "Unable to commit read transaction for pool %d", pool.ID)
+			}
+		}(tx)
 	}
 
 	ps, err := pool.PoolProperties()
